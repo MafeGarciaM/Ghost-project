1131fac9fada2412b6138ec5586150fdafa40710
Felipe Leyva <d.leyvad@uniandes.edu.co>
2022-05-22 23:55:59 -0500

    pages data cypress

--- cypress/cypress/integration/pages_data.spec.js
+++ cypress/cypress/integration/pages_data.spec.js
@@ -4,6 +4,7 @@ import { PagesPage, LoginPage } from './pages';
 var gversion = '';
 var data_source = '';
 var pagesInfoPositive = [];
+var pagesInfoNegative = [];
 
 const { faker } = require('@faker-js/faker');
 
@@ -12,19 +13,31 @@ describe('2e2 ghost app pages', () => {
   before(() => {
 
     data_source = Cypress.env('data_source');
+    console.log('DATA SOURCE:', data_source)
 
     switch(data_source) {
       case 'PRIORI':
-        cy.fixture('pages_priori').then((page) => {
-          pagesInfoPositive = page
+        cy.fixture('pages_priori_positive').then((pagePositive) => {
+          console.log('POSITIVO:', pagePositive);
+          return pagesInfoPositive = pagePositive;
+        })
+        cy.fixture('pages_priori_negative').then((pageNegative) => {
+          console.log('NEGATIVO:', pageNegative);
+          return pagesInfoNegative = pageNegative;
         })
       break;
       case 'PSEUDO':
         faker.seed(1)
-        pagesInfoPositive = Cypress._.range(1,20).map((_, k) => {
+        pagesInfoPositive = Cypress._.range(1,500).map((_, k) => {
           return {
             pageTitle: faker.random.words(),
             pageBody: faker.lorem.paragraphs()
+          }
+        })
+        pagesInfoNegative = Cypress._.range(1,500).map((_, k) => {
+          return {
+            pageTitle: faker.random.words(200),
+            pageBody: faker.lorem.paragraphs(500)
           }
         })
       break;
@@ -33,6 +46,12 @@ describe('2e2 ghost app pages', () => {
           return {
             pageTitle: faker.random.words(),
             pageBody: faker.lorem.paragraphs()
+          }
+        })
+        pagesInfoNegative = Cypress._.range(1,500).map((_, k) => {
+          return {
+            pageTitle: faker.random.words(200),
+            pageBody: faker.lorem.paragraphs(500)
           }
         })
       break;
@@ -45,101 +64,71 @@ describe('2e2 ghost app pages', () => {
 
   beforeEach(() => {
     gversion = Cypress.env('ghost_version');
-    
     cy.visit(Cypress.env('login_url'))
     self.loginPage = new LoginPage();
     self.pagesPage = new PagesPage();
     loginPage.login(Cypress.env('username'), Cypress.env('password'))
   })
-
-  it ('prueba', () => {
-    console.log('MIS PAGINAS', pagesInfoPositive)
-    console.log('PRIMERA PAGINA:', pagesInfoPositive[0])
-    console.log('SEGUNDA PAGINA:', pagesInfoPositive[1])
-  })
   
   // Borrar todas las paginas antes de ejecutar
   
-  it('test create new page', () => {
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-1')
-    cy.wait(3000)
-    self.pagesPage.clickNewPageButton()
-    cy.wait(2000)
-    self.pagesPage.typePageName(pagesInfoPositive[4].pageTitle)
-    self.pagesPage.typePageText('This is a test page created by Cypress.')
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-2')
-    cy.wait(2000)
-    self.pagesPage.clickPublishMenu()
-    cy.wait(2000)
-    self.pagesPage.clickPublishButton()
-    cy.wait(2000)
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-3')
-    cy.wait(1000)
-    self.pagesPage.visitPage('test-page')
-    cy.wait(5000)
-    gversion === '3.42' ? self.pagesPage.assertPageExistsOld('Test Page') : self.pagesPage.assertPageExistsNew('Test Page')
-    cy.wait(2000)
-  })
+  var i = 0;
+  Cypress._.times(5,
+    () => {
+      it.only(`test create new page element, atempt ${i + 1}`, () => {
+        self.pagesPage.clickPageSectionButton()
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-1')
+        cy.wait(3000)
+        self.pagesPage.clickNewPageButton()
+        cy.wait(2000)
+        self.pagesPage.typePageName(pagesInfoPositive[i].pageTitle)
+        self.pagesPage.typePageText(pagesInfoPositive[i].pageBody)
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-2')
+        cy.wait(2000)
+        self.pagesPage.clickPublishMenu()
+        cy.wait(2000)
+        self.pagesPage.clickPublishButton()
+        cy.wait(2000)
+        self.pagesPage.clickPageSectionButton()
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-3')
+        cy.wait(1000)
+        self.pagesPage.visitPage('test-page')
+        cy.wait(5000)
+        gversion === '3.42' ? self.pagesPage.assertPageExistsOld('Test Page') : self.pagesPage.assertPageExistsNew('Test Page')
+        cy.wait(2000)
+      });
+      i++;
+    }
+  )
 
-it('test create new page with emptu ttile', () => {
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-1')
-    cy.wait(3000)
-    self.pagesPage.clickNewPageButton()
-    cy.wait(2000)
-    self.pagesPage.typePageName(pagesInfoPositive[0].pageTitle)
-    self.pagesPage.typePageText('This is a test page created by Cypress.')
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-2')
-    cy.wait(2000)
-    self.pagesPage.clickPublishMenu()
-    cy.wait(2000)
-    self.pagesPage.clickPublishButton()
-    cy.wait(2000)
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-3')
-    cy.wait(1000)
-    self.pagesPage.visitPage('test-page')
-    cy.wait(5000)
-    gversion === '3.42' ? self.pagesPage.assertPageExistsOld('Test Page') : self.pagesPage.assertPageExistsNew('Test Page')
-    cy.wait(2000)
-  })
-  
-  it('test create delete page', () => {
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-delete-page-step-1')
-    cy.wait(2000)
-    self.pagesPage.clickNewPageButton()
-    cy.wait(2000)
-    self.pagesPage.typePageName('Deletable Page')
-    self.pagesPage.typePageText('This is a deletable page created by Cypress.')
-    self.pagesPage.takeScreenshot('ghost-3-42-delete-page-step-2')
-    cy.wait(2000)
-    self.pagesPage.clickPublishMenu()
-    cy.wait(2000)
-    self.pagesPage.clickPublishButton()
-    cy.wait(2000)
-    self.pagesPage.clickPageSectionButton()
-    self.pagesPage.takeScreenshot('ghost-3-42-delete-page-step-3')
-    cy.wait(2000)
-    
-    self.pagesPage.clickFirstPageItem()
-    cy.wait(2000)
-    gversion === '3.42' ? self.pagesPage.clickPageMenuOld() : self.pagesPage.clickPageMenuNew()
-    self.pagesPage.takeScreenshot('ghost-3-42-delete-page-step-4')
-    cy.wait(2000)
-    self.pagesPage.clickPageDeleteButton()
-    cy.wait(2000)
-    self.pagesPage.clickConfirmDeleteButton()
-    cy.wait(2000)
-    self.pagesPage.takeScreenshot('ghost-3-42-delete-page-step-5')
-    cy.wait(2000)
-    self.pagesPage.visitPage('deletable-page')
-    cy.wait(5000)
-    self.pagesPage.assertPageNotExists()
-    cy.wait(2000)
-  })
+  var i = 0;
+  Cypress._.times(5,
+    () => {
+      it.only(`test create new page with invalid data, atempt ${i + 1} `, () => {
+        self.pagesPage.clickPageSectionButton()
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-1')
+        cy.wait(3000)
+        self.pagesPage.clickNewPageButton()
+        cy.wait(2000)
+        self.pagesPage.typePageName(pagesInfoNegative[i].pageTitle)
+        self.pagesPage.typePageText(pagesInfoNegative[i].pageBody)
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-2')
+        cy.wait(2000)
+        self.pagesPage.clickPublishMenu()
+        cy.wait(2000)
+        self.pagesPage.clickPublishButton()
+        cy.wait(2000)
+        self.pagesPage.clickPageSectionButton()
+        self.pagesPage.takeScreenshot('ghost-3-42-create-page-step-3')
+        cy.wait(1000)
+        self.pagesPage.visitPage('test-page')
+        cy.wait(5000)
+        gversion === '3.42' ? self.pagesPage.assertPageExistsOld('Test Page') : self.pagesPage.assertPageExistsNew('Test Page')
+        cy.wait(2000)
+      });
+      i++;
+    }
+  )
   
   it('test create and edit page', () => {
     self.pagesPage.clickPageSectionButton()
